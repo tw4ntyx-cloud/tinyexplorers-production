a33a472d4e8c206ec1084e6569053a76b1fbab73
tw4ntyx-cloud <inboxtwentyx@gmail.com>
2026-08-03 18:53:07 +0000

    changes

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const API = `${BACKEND_URL}/api`;
+export const API = BACKEND_URL ? `${BACKEND_URL}/api` : "/api";
 
 export const api = axios.create({
   baseURL: API,
